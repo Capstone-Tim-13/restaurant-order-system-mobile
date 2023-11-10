02266c9dba443e7b12f3ef9bdd1252dd47eae39f
David C <211112742@students.mikroskil.ac.id>
2023-11-10 16:41:48 +0700

    Update onboarding2_page.dart

--- lib/pages/login/onboarding2_page.dart
+++ lib/pages/login/onboarding2_page.dart
@@ -1,1 +1,32 @@
-//  David Hui
+void main() => runApp(MaterialApp(
+      home: SplashScreen2(),
+    ));
+
+class SplashScreen2 extends StatefulWidget {
+  const SplashScreen2({super.key});
+
+  @override
+  State<SplashScreen2> createState() => _SplashScreen2State();
+}
+
+class _SplashScreen2State extends State<SplashScreen2> {
+
+@override
+void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (context) => HomeScreen(),
+      ));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Splash Screen"),
+      ),
+    );
+  }
+}
