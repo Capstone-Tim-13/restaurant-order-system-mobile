39850143be7ac7cda6e30ce29ae973e8a4d7cb75
Katkatarr <melodyofagatha@gmail.com>
2023-11-11 21:26:12 +0700

    splash_screen_5

--- lib/pages/login/onboarding5_page.dart
+++ lib/pages/login/onboarding5_page.dart
@@ -1,210 +1,139 @@
 // Katarina
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+class Splashscreen5 extends StatefulWidget {
+  static const String routeName = '/Splashscreen5';
+  const Splashscreen5({super.key});
 
-class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '/splashscreen5',
-      routes: {
-        '/splashscreen5': (context) => MySplashScreen(),
-      },
-    );
-  }
+  State<Splashscreen5> createState() => _Splashscreen5State();
 }
 
-class MySplashScreen extends StatelessWidget {
+class _Splashscreen5State extends State<Splashscreen5> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(top: 40), // Add a top margin of 40
+      body: Container(
         child: Container(
-          width: 376,
-          height: 376,
+          decoration: BoxDecoration(
+            color: Color(0xffffffff),
+          ),
           child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Align(
-                alignment: Alignment(
-                    -1.0 + (9 / 376),
-                    -1.0 +
-                        (137 / 376)), // Pindahkan gambar ke x = 9 dan y = 137
-                child: Padding(
-                  padding: EdgeInsets.only(
-                    top: 127,
-                    left: 30,
-                  ), // Add padding to the top of the image
-                  child: Container(
-                    width: 376,
-                    height: 376,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/images/splashscreen5.png"),
-                        fit: BoxFit.fill,
-                      ),
-                    ),
-                  ),
+              Center(
+                child: Image.asset(
+                  'assets/images/splashscreen5.png',
+                  width: 310,
+                  height: 310,
                 ),
               ),
-              SizedBox(height: 0), // Jarak antara gambar dan teks pertama
-              MyText(
-                text: 'Delivery offers',
-                color: Color(0xFF010F07),
-                fontSize: 32,
-                fontWeight: FontWeight.w600,
-                letterSpacing: 0.2,
-              ),
-              SizedBox(height: 5), // Jarak antara teks pertama dan teks kedua
-              MyText(
-                text:
-                    'Memberikan penawaran untuk dikirim ke tujuan mu masing-masing, sangat menghemat waktumu',
-                color: Color(0xFF868686),
-                fontSize: 16,
-                fontWeight: FontWeight.w100,
-              ),
               SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: List.generate(5, (index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: CircleAvatar(
-                      radius: 5,
-                      backgroundColor: Color(0xFF727272),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 0, 0, 19),
+                      child: Text(
+                        'Delivery offers',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'Font Awesome 5 Brands',
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          height: 1.2666666667,
+                          letterSpacing: 0.3199999928,
+                          color: Color(0xff010f07),
+                        ),
+                      ),
                     ),
-                  );
-                }),
+                    Container(
+                      constraints: BoxConstraints(
+                        maxWidth: 300,
+                      ),
+                      child: Text(
+                        'Memberikan penawaran untuk dikirim ke tujuan mu masing-masing, sangat menghemat waktumu.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'Yu Gothic UI',
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          height: 1.5,
+                          color: Color(0xff868686),
+                          letterSpacing: 0.4,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.circle, color: Colors.grey, size: 7),
+                        SizedBox(width: 2),
+                        Icon(Icons.circle, color: Colors.grey, size: 7),
+                        SizedBox(width: 2),
+                        Icon(Icons.circle, color: Color(0xff783525), size: 7),
+                        SizedBox(width: 2),
+                        Icon(Icons.circle, color: Colors.grey, size: 7),
+                      ],
+                    ),
+                    SizedBox(height: 35),
+                    TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        padding: EdgeInsets.zero,
+                      ),
+                      child: Container(
+                        width: double.infinity,
+                        height: 45,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Color(0xff783525),
+                            borderRadius: BorderRadius.circular(24),
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Selanjutnya',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700,
+                                height: 1.25,
+                                letterSpacing: 0.6000000238,
+                                color: Color(0xffffffff),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              SizedBox(height: 20), // Jarak antara teks kedua dan tombol
-              MyButton(
-                text: 'Selanjutnya',
-                color: Color(0xFF783524),
-                textColor: Colors.white,
-                fontSize: 16,
-                fontWeight: FontWeight.w700,
-                letterSpacing: 0.60,
-              ),
-              SizedBox(height: 10), // Jarak antara tombol dan teks "Lewati"
-              MyText(
-                text: 'Lewati',
-                color: Color(0xFF727272),
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
+              TextButton(
+                onPressed: () {
+                  // Handle "Lewati" button press event
+                },
+                child: Text(
+                  'Lewati',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Yu Gothic UI',
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    height: 1.5,
+                    color: Color(0xff868686),
+                    letterSpacing: 0.4,
+                  ),
+                ),
               ),
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class MyText extends StatelessWidget {
-  final String text;
-  final Color color;
-  final double fontSize;
-  final FontWeight fontWeight;
-  final double letterSpacing;
-
-  MyText({
-    required this.text,
-    required this.color,
-    required this.fontSize,
-    required this.fontWeight,
-    this.letterSpacing = 0.0,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 333,
-      height: 78,
-      child: Text(
-        text,
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          color: color,
-          fontSize: fontSize,
-          fontFamily: 'Poppins',
-          //fontWeight: fontWeight,
-          letterSpacing: letterSpacing,
-        ),
-      ),
-    );
-  }
-}
-
-class MyButton extends StatelessWidget {
-  final String text;
-  final Color color;
-  final Color textColor;
-  final double fontSize;
-  final FontWeight fontWeight;
-  final double letterSpacing;
-
-  MyButton({
-    required this.text,
-    required this.color,
-    required this.textColor,
-    required this.fontSize,
-    required this.fontWeight,
-    this.letterSpacing = 0.0,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 335,
-      height: 48,
-      decoration: ShapeDecoration(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(321),
-        ),
-      ),
-      child: Stack(
-        children: [
-          Positioned(
-            left: 0,
-            top: 0,
-            child: Container(
-              width: 335,
-              height: 48,
-              decoration: ShapeDecoration(
-                color: color,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(37),
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            left: 60,
-            top: 10,
-            bottom: 0,
-            child: SizedBox(
-              width: 215,
-              height: 5,
-              child: Text(
-                text,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: textColor,
-                  fontSize: fontSize,
-                  fontFamily: 'Poppins',
-                  //fontWeight: fontWeight,
-                  letterSpacing: letterSpacing,
-                ),
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }
