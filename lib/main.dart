9bd37bff213217f9b946f4e0c8162d677ba6f138
RachaelNathasya <142284806+RachaelNathasya@users.noreply.github.com>
2023-11-18 16:23:42 +0700

    homepage

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-     initialRoute: '/',
+      initialRoute: '/',
       routes: {
         '/': (context) => const MyHomePage(),
         '/login': (context) => const MyHomePage(),
