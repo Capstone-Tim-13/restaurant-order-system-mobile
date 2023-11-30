7c29856b2a66cf53a24447341677c402aed46e54
Adeali7 <adealiindra@gmail.com>
2023-11-30 12:18:57 +0700

    update progres

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,9 @@
-
-
-import 'package:capstone_restaurant/pages/bantuan/rating_page.dart';
-import 'package:capstone_restaurant/pages/profile/profile_page.dart';
-import 'package:capstone_restaurant/pages/profile/change_password_page.dart';
-import 'package:capstone_restaurant/pages/profile/my_account_page.dart';
 import 'package:capstone_restaurant/pages/login/forget_password_page.dart';
 import 'package:capstone_restaurant/pages/login/login_page.dart';
 import 'package:capstone_restaurant/pages/login/onboarding_page.dart';
 import 'package:capstone_restaurant/pages/login/register_page.dart';
-import 'package:capstone_restaurant/pages/pesanan/searching_page.dart';
+import 'package:capstone_restaurant/pages/pesanan/pesanan_page.dart';
 import 'package:capstone_restaurant/pages/splash_screen.dart';
-import 'package:capstone_restaurant/pages/pesanan/popup_menu.dart';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -29,24 +21,18 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        // useMaterial3: true,
+        useMaterial3: true,
       ),
-      initialRoute: '/',
+      initialRoute: '/orderpage',
       routes: {
-
-                     
-         
-   
-        '/': (context) => const ProfilePage(),
-        '/onboarding': (context) => const OnboardingPage(),
-        '/login': (context) => const LoginPage(),
-        '/register': (context) => const RegisterPage(),
-        '/search': (context) => const SearchPage(),
-        '/forgetPassword': (context) => const ForgetPassword(),
-        '/popupmenupage': (context) => PopupMenuPage(),
-        '/feedbackPage': (context) =>const FeedbackPage(),
-
+        // '/': (context) => const Splash(),
+        // '/onboarding': (context) => const OnboardingPage(),
+        // '/login': (context) => const LoginPage(),
+        // '/register': (context) => const RegisterPage(),
+        // '/forgetPassword': (context) => const ForgetPassword(),
+        '/orderpage': (context) => const OrderPage(),
       },
     );
   }
 }
+
---
+++ lib/pages/pesanan/pesanan_page.dart
@@ -0,0 +1,97 @@
+// Ali
+
+import "package:capstone_restaurant/pages/pesanan/ongoing_widget.dart";
+import "package:capstone_restaurant/pages/pesanan/riwayat_widget.dart";
+import "package:flutter/material.dart";
+
+
+class OrderPage extends StatefulWidget {
+  const OrderPage({Key? key}) : super(key: key);
+
+  @override
+  State<OrderPage> createState() => _OrderPageState();
+}
+
+class _OrderPageState extends State<OrderPage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Image.asset('assets/images/back_1.png')
+              ),
+             const Padding(padding: EdgeInsets.only(left: 8),
+              child: Text(
+                "Pesanan",
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                fontWeight: FontWeight.w500,
+                color: Color(0xff000000),
+                 ),
+                ),
+              )
+            ],
+          ),
+         bottom:const TabBar(
+              labelStyle: TextStyle(
+              fontFamily:'Poppins',
+              fontSize: 14,
+              color: Color(0xff4D0F27),
+              ),
+              unselectedLabelColor: Color(0xff737373),
+             labelPadding: EdgeInsets.symmetric(horizontal: 15),
+              indicatorColor: Color(0xff4D0F27),
+              
+              
+
+              tabs: [
+                Tab(text: "Berlangsung"),
+                Tab(text: "Riwayat"),
+                Tab(text: "Dibatalkan"),
+              ],
+            ), 
+        ),
+          body: TabBarView(children:[
+              ongoingWidget(context), 
+              riwayatWidget(context),
+              Container(color: Colors.blue),
+           
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+// Widget ongoingWidget(BuildContext context) {
+//     return  SizedBox(
+//      width: double.infinity,
+//     height: 251,
+//     child: Stack(
+//       children: [
+//         Card(
+//           clipBehavior: Clip.antiAliasWithSaveLayer,
+//           child: Column(
+//             children: [
+//               SizedBox(
+//                 width: double.infinity,
+//                 height: 200,
+//                 child: Image.asset('assets/images/home/orderPage/ongoing/ongoingMenu.png',
+//                     fit: BoxFit.cover,),
+//               )
+//             ],
+//           ),
+//         )
+//       ],
+//     ),
+    
+//    );
+//  }
